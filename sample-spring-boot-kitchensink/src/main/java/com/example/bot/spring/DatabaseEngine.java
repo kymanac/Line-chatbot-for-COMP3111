8a771926e1b58627cec1310e0d37167655b4669c
kimberley <kimberley@kimberleydeMacBook-Pro.local>
2017-09-19 13:06:39 +0800

    Test #1

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/DatabaseEngine.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/DatabaseEngine.java
@@ -29,12 +29,13 @@ public class DatabaseEngine {
 		BufferedReader br = null;
 		InputStreamReader isr = null;
 		try {
+			String sCurrentLine;
 			isr = new InputStreamReader(
                     this.getClass().getResourceAsStream(FILENAME));
 			br = new BufferedReader(isr);
-			String sCurrentLine;
 			
-			while (result != null && (sCurrentLine = br.readLine()) != null) {
+			
+			while ((sCurrentLine = br.readLine()) != null) {
 				String[] parts = sCurrentLine.split(":");
 				if (text.toLowerCase().equals(parts[0].toLowerCase())) {
 					result = parts[1];
@@ -60,3 +61,41 @@ public class DatabaseEngine {
 	private final String FILENAME = "/static/database.txt";
 
 }
+
+//public class DatabaseEngine {
+//	String search(String text) throws Exception {
+//		String result = null;
+//		BufferedReader br = null;
+//		InputStreamReader isr = null;
+//		try {
+//			isr = new InputStreamReader(
+//                    this.getClass().getResourceAsStream(FILENAME));
+//			br = new BufferedReader(isr);
+//			String sCurrentLine;
+//			
+//			while (result != null && (sCurrentLine = br.readLine()) != null) {
+//				String[] parts = sCurrentLine.split(":");
+//				if (text.toLowerCase().equals(parts[0].toLowerCase())) {
+//					result = parts[1];
+//				}
+//			}
+//		} catch (IOException e) {
+//			log.info("IOException while reading file: {}", e.toString());
+//		} finally {
+//			try {
+//				if (br != null)
+//					br.close();
+//				if (isr != null)
+//					isr.close();
+//			} catch (IOException ex) {
+//				log.info("IOException while closing file: {}", ex.toString());
+//			}
+//		}
+//		if (result != null)
+//			return result;
+//		throw new Exception("NOT FOUND");
+//    }
+//	
+//	private final String FILENAME = "/static/database.txt";
+//
+//}
